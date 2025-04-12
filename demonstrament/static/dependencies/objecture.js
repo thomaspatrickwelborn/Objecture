1ca8a7973ac6745b1931213af0076a3a3902461f
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-04-12 11:29:52 -0700

    Schema Class Property Definitions

--- demonstrament/static/dependencies/objecture.js
+++ demonstrament/static/dependencies/objecture.js
@@ -1696,72 +1696,168 @@ var Options$1 = (...$options) => Object.assign({
 const { typedObjectLiteral: typedObjectLiteral$7, typeOf: typeOf$3 } = index;
 
 class Schema extends EventTarget {
-  #properties
-  options
-  #type
-  #context
-  #parent
-  #key
-  #path
-  #requiredProperties
-  #requiredPropertiesSize
   constructor($properties = {}, $options = {}) {
     super();
-    this.#properties = $properties;
-    this.options = Options$1($options);
-  }
-  get type() {
-    if(this.#type !== undefined) return this.#type
-    this.#type = typeOf$3(this.#properties);
-    return this.#type
-  }
-  get parent() {
-    if(this.#parent !== undefined)  return this.#parent
-    this.#parent = (this.options.parent) ? this.options.parent : null;
-    return this.#parent
-  }
-  get root() {
-    let root = this;
-    iterateParents: 
-    while(root) {
-      if([undefined, null].includes(root.parent)) { break iterateParents }
-      root = root.parent;
-    }
-    return root
-  }
-  get key() {
-    if(this.#key !== undefined) { return this.#key }
-    if(this.path) { this.#key = this.path.split('.').pop(); }
-    else { this.#key = null; }
-    return this.#key
-  }
-  get path() {
-    if(this.#path !== undefined)  return this.#path
-    this.#path = (this.options.path)
-      ? String(this.options.path)
-      : null;
-    return this.#path
-  }
-  get required() { return this.options.required }
-  get requiredProperties() {
-    if(this.#requiredProperties !== undefined) return this.#requiredProperties
-    let requiredProperties = typedObjectLiteral$7(this.type);
-    for(const [$propertyKey, $propertyDefinition] of Object.entries(this.context)) {
-      if($propertyDefinition.required?.value === true) { requiredProperties[$propertyKey] = $propertyDefinition; }
-    }
-    this.#requiredProperties = requiredProperties;
-    return this.#requiredProperties
-  }
-  get requiredPropertiesSize() {
-    if(this.#requiredPropertiesSize !== undefined) return this.#requiredPropertiesSize
-    this.#requiredPropertiesSize = Object.keys(this.requiredProperties).length;
-    return this.#requiredPropertiesSize
-  }
-  get verificationType() { return this.options.verificationType }
-  get context() {
-    if(this.#context !== undefined) return this.#context
-    this.#context = new Context(this.#properties, this);
-    return this.#context
+    Object.defineProperty(this, 'options', { configurable: true, get() {
+      const options = Options$1($options);
+      Object.defineProperty(this, 'options', { value: options });
+      return options
+    }});
+    Object.defineProperty(this, 'type', { configurable: true, get() { 
+      const type = typeOf$3($properties);
+      Object.defineProperty(this, 'options', { value: type });
+      return type
+    }});
+    Object.defineProperty(this, 'parent', { configurable: true, get() {
+      const parent = (this.options.parent) ? this.options.parent : null;
+      Object.defineProperty(this, 'parent', { value: parent });
+      return parent
+    } });
+    Object.defineProperty(this, 'root', { configurable: true, get() {
+      let root = this;
+      iterateParents: 
+      while(root) {
+        if([undefined, null].includes(root.parent)) { break iterateParents }
+        root = root.parent;
+      }
+      return root
+    } });
+    Object.defineProperty(this, 'key', { configurable: true, get() {
+      const key = (this.path) ? this.path.split('.').pop() : null;
+      Object.defineProperty(this, 'key', { value: key });
+      return key
+    } });
+    Object.defineProperty(this, 'path', { configurable: true, get() {
+      const path = (this.options.path)
+        ? String(this.options.path)
+        : null;
+      Object.defineProperty(this, 'path', { value: path });
+      return path
+    } });
+    Object.defineProperty(this, 'required', { configurable: true, get() {
+      const required = (this.options.required) ? this.options.required : null;
+      Object.defineProperty(this, 'required', { value: required });
+      return required
+    } });
+    Object.defineProperty(this, 'requiredProperties', { configurable: true, get() {
+      const requiredProperties = typedObjectLiteral$7(this.type);
+      for(const [$propertyKey, $propertyDefinition] of Object.entries(this.context)) {
+        if($propertyDefinition.required?.value === true) { requiredProperties[$propertyKey] = $propertyDefinition; }
+      }
+      Object.defineProperty(this, 'requiredProperties', { value: requiredProperties });
+      return requiredProperties
+    } });
+
+    Object.defineProperty(this, 'requiredPropertiesSize', { configurable: true, get() {
+      const requiredPropertiesSize = Object.keys(this.requiredProperties).length;
+      Object.defineProperty(this, 'requiredPropertiesSize', { value: requiredPropertiesSize });
+      return requiredPropertiesSize
+    } });
+
+    Object.defineProperty(this, 'verificationType', { configurable: true, get() {
+      const verificationType = this.options.verificationType;
+      Object.defineProperty(this, 'verificationType', { value: verificationType });
+      return verificationType
+    } });
+
+    Object.defineProperty(this, 'context', { configurable: true, get() {
+      const context = new Context($properties, this);
+      Object.defineProperty(this, 'context', { value: context });
+      return context
+    } });
+
+    Object.defineProperty(this, 'validate', { value: function() {
+      const { $sourceName, $source, $target } = this.#parseValidateArguments(...arguments);
+      const validation = new Validation({
+        definition: this.context,
+        path: this.path,
+        key: $sourceName, 
+        value: $source,
+        properties: typedObjectLiteral$7(this.type),
+      });
+      const sourceProperties = Object.entries($source);
+      let sourcePropertyIndex = 0;
+      let deadvancedRequiredProperties = [];
+      // Iterate Model Properties 
+      while(sourcePropertyIndex < sourceProperties.length) {
+        const [$sourceKey, $sourceValue] = sourceProperties[sourcePropertyIndex];
+        const propertyValidation = this.validateProperty($sourceKey, $sourceValue, $source, $target);
+        const deadvancedRequiredPropertyValidation = propertyValidation.deadvance.filter(
+          ($verification) => $verification.type === 'required'
+        );
+        validation.properties[$sourceKey] = propertyValidation;
+        if(propertyValidation.valid === true) { validation.advance.push(propertyValidation); } 
+        else if(propertyValidation.valid === false) { validation.deadvance.push(propertyValidation); } 
+        else if(propertyValidation.valid === undefined) { validation.unadvance.push(propertyValidation );}
+        deadvancedRequiredProperties = deadvancedRequiredProperties.concat(deadvancedRequiredPropertyValidation);
+        sourcePropertyIndex++;
+      }
+      if(this.required === true) {
+        if(validation.deadvance.length) { validation.valid = false; }
+        else if(validation.advance.length) { validation.valid = true; }
+        else if(validation.unadvance.length) { validation.valid = undefined; }
+        else { validation.valid = false; }
+      }
+      else if(this.required === false) {
+        if(deadvancedRequiredProperties.length) { validation.valid = false; }
+        else if(validation.advance.length) { validation.valid = true; }
+        else if(validation.deadvance.length) { validation.valid = false; }
+        else if(validation.unadvance.length) { validation.valid = undefined; }
+        else { validation.valid = false; }
+      }
+      return validation
+    } });
+
+    Object.defineProperty(this, 'validateProperty', { value: function() {
+      const { $key, $value, $source, $target } = this.#parseValidatePropertyArguments(...arguments);
+      let propertyDefinition;
+      if(this.type === 'array') { propertyDefinition = this.context[0]; }
+      else if(this.type === 'object') { propertyDefinition = this.context[$key]; }
+      const { path } = this;
+      const propertyValidationPath = (path) ? [path, $key].join('.') : $key;
+      const propertyValidation = new Validation({
+        // type: this.required,
+        definition: propertyDefinition,
+        path: propertyValidationPath,
+        key: $key,
+        value: $value,
+      });
+      // Context Value: Undefined
+      if(propertyDefinition === undefined) {
+        const verification = new Verification({
+          type: null,
+          definition: null,
+          key: $key,
+          value: $value,
+        }, this);
+        verification.pass = false;
+        propertyValidation.unadvance.push(verification);
+      }
+      // Context Value: Object
+      else if(propertyDefinition instanceof Schema) {
+        let validation;
+        if($target && $target[$key]) { validation = propertyDefinition.validate($key, $value, $target[$key]); }
+        else { validation = propertyDefinition.validate($key, $value); }
+        if(validation.valid === true) { propertyValidation.advance.push(validation); }
+        else if(validation.valid === false) { propertyValidation.deadvance.push(validation); }
+        else if(validation.valid === undefined) { propertyValidation.unadvance.push(validation); }
+      }
+      // Context Value: Primitive
+      else {
+        iterateContextValueValidators:
+        for(const [$validatorIndex, $validator] of Object.entries(propertyDefinition.validators)) {
+          const verification = $validator.validate($key, $value, $source, $target);
+          if(verification.pass === true) { propertyValidation.advance.push(verification); }
+          else if(verification.pass === false) { propertyValidation.deadvance.push(verification); }
+          else if(verification.pass === undefined) { propertyValidation.unadvance.push(verification); }
+          if(this.verificationType === 'one' && propertyValidation.deadvance.length) { break iterateContextValueValidators }
+        }
+      }
+      if(propertyValidation.deadvance.length) { propertyValidation.valid = false; }
+      else if(propertyValidation.advance.length) { propertyValidation.valid = true; }
+      else if(propertyValidation.unadvance.length) { propertyValidation.valid = false; }
+      return propertyValidation
+    } });
   }
   #parseValidateArguments() {
     let $arguments = [...arguments];
@@ -1780,47 +1876,6 @@ class Schema extends EventTarget {
     }
     return { $sourceName, $source, $target }
   }
-  validate() {
-    const { $sourceName, $source, $target } = this.#parseValidateArguments(...arguments);
-    const validation = new Validation({
-      definition: this.context,
-      path: this.path,
-      key: $sourceName, 
-      value: $source,
-      properties: typedObjectLiteral$7(this.type),
-    });
-    const sourceProperties = Object.entries($source);
-    let sourcePropertyIndex = 0;
-    let deadvancedRequiredProperties = [];
-    // Iterate Model Properties 
-    while(sourcePropertyIndex < sourceProperties.length) {
-      const [$sourceKey, $sourceValue] = sourceProperties[sourcePropertyIndex];
-      const propertyValidation = this.validateProperty($sourceKey, $sourceValue, $source, $target);
-      const deadvancedRequiredPropertyValidation = propertyValidation.deadvance.filter(
-        ($verification) => $verification.type === 'required'
-      );
-      validation.properties[$sourceKey] = propertyValidation;
-      if(propertyValidation.valid === true) { validation.advance.push(propertyValidation); } 
-      else if(propertyValidation.valid === false) { validation.deadvance.push(propertyValidation); } 
-      else if(propertyValidation.valid === undefined) { validation.unadvance.push(propertyValidation );}
-      deadvancedRequiredProperties = deadvancedRequiredProperties.concat(deadvancedRequiredPropertyValidation);
-      sourcePropertyIndex++;
-    }
-    if(this.required === true) {
-      if(validation.deadvance.length) { validation.valid = false; }
-      else if(validation.advance.length) { validation.valid = true; }
-      else if(validation.unadvance.length) { validation.valid = undefined; }
-      else { validation.valid = false; }
-    }
-    else if(this.required === false) {
-      if(deadvancedRequiredProperties.length) { validation.valid = false; }
-      else if(validation.advance.length) { validation.valid = true; }
-      else if(validation.deadvance.length) { validation.valid = false; }
-      else if(validation.unadvance.length) { validation.valid = undefined; }
-      else { validation.valid = false; }
-    }
-    return validation
-  }
   #parseValidatePropertyArguments() {
     let $arguments = [...arguments];
     let [$key, $value, $source, $target] = $arguments;
@@ -1830,56 +1885,6 @@ class Schema extends EventTarget {
     const $targetIsModelClassInstance = ($target instanceof Model);
     $target = ($targetIsModelClassInstance) ? $target.valueOf() : $target;
     return { $key, $value, $source, $target }
-  }
-  validateProperty() {
-    const { $key, $value, $source, $target } = this.#parseValidatePropertyArguments(...arguments);
-    let propertyDefinition;
-    if(this.type === 'array') { propertyDefinition = this.context[0]; }
-    else if(this.type === 'object') { propertyDefinition = this.context[$key]; }
-    const { path } = this;
-    const propertyValidationPath = (path) ? [path, $key].join('.') : $key;
-    const propertyValidation = new Validation({
-      // type: this.required,
-      definition: propertyDefinition,
-      path: propertyValidationPath,
-      key: $key,
-      value: $value,
-    });
-    // Context Value: Undefined
-    if(propertyDefinition === undefined) {
-      const verification = new Verification({
-        type: null,
-        definition: null,
-        key: $key,
-        value: $value,
-      }, this);
-      verification.pass = false;
-      propertyValidation.unadvance.push(verification);
-    }
-    // Context Value: Object
-    else if(propertyDefinition instanceof Schema) {
-      let validation;
-      if($target && $target[$key]) { validation = propertyDefinition.validate($key, $value, $target[$key]); }
-      else { validation = propertyDefinition.validate($key, $value); }
-      if(validation.valid === true) { propertyValidation.advance.push(validation); }
-      else if(validation.valid === false) { propertyValidation.deadvance.push(validation); }
-      else if(validation.valid === undefined) { propertyValidation.unadvance.push(validation); }
-    }
-    // Context Value: Primitive
-    else {
-      iterateContextValueValidators:
-      for(const [$validatorIndex, $validator] of Object.entries(propertyDefinition.validators)) {
-        const verification = $validator.validate($key, $value, $source, $target);
-        if(verification.pass === true) { propertyValidation.advance.push(verification); }
-        else if(verification.pass === false) { propertyValidation.deadvance.push(verification); }
-        else if(verification.pass === undefined) { propertyValidation.unadvance.push(verification); }
-        if(this.verificationType === 'one' && propertyValidation.deadvance.length) { break iterateContextValueValidators }
-      }
-    }
-    if(propertyValidation.deadvance.length) { propertyValidation.valid = false; }
-    else if(propertyValidation.advance.length) { propertyValidation.valid = true; }
-    else if(propertyValidation.unadvance.length) { propertyValidation.valid = false; }
-    return propertyValidation
   }
 }
 
@@ -2098,24 +2103,24 @@ let ValidatorEvent$1 = class ValidatorEvent extends CustomEvent {
   constructor($type, $settings, $model) {
     super($type);
     Object.defineProperties(this, {
-      'key': { configurable: true, enumerable: true, get () {
+      'key': { configurable: true, get () {
         const key = $settings.key;
-        Object.defineProperty(this, 'key', { enumerable: true, value: key });
+        Object.defineProperty(this, 'key', { value: key });
         return key
       } },
-      'path': { configurable: true, enumerable: true, get () {
+      'path': { configurable: true, get () {
         const path = $settings.path;
-        Object.defineProperty(this, 'path', { enumerable: true, value: path });
+        Object.defineProperty(this, 'path', { value: path });
         return path
       } },
-      'value': { configurable: true, enumerable: true, get () {
+      'value': { configurable: true, get () {
         const value = $settings.value;
-        Object.defineProperty(this, 'value', { enumerable: true, value: value, });
+        Object.defineProperty(this, 'value', { value: value, });
         return value
       } },
-      'valid': { configurable: true, enumerable: true, get () {
+      'valid': { configurable: true, get () {
         const valid = $settings.valid;
-        Object.defineProperty(this, 'valid', { enumerable: true, value: valid });
+        Object.defineProperty(this, 'valid', { value: valid });
         return valid
       } },
 
--- development/model/events/validator/index.js
+++ development/model/events/validator/index.js
@@ -2,24 +2,24 @@ export default class ValidatorEvent extends CustomEvent {
   constructor($type, $settings, $model) {
     super($type)
     Object.defineProperties(this, {
-      'key': { configurable: true, enumerable: true, get () {
+      'key': { configurable: true, get () {
         const key = $settings.key
-        Object.defineProperty(this, 'key', { enumerable: true, value: key })
+        Object.defineProperty(this, 'key', { value: key })
         return key
       } },
-      'path': { configurable: true, enumerable: true, get () {
+      'path': { configurable: true, get () {
         const path = $settings.path
-        Object.defineProperty(this, 'path', { enumerable: true, value: path })
+        Object.defineProperty(this, 'path', { value: path })
         return path
       } },
-      'value': { configurable: true, enumerable: true, get () {
+      'value': { configurable: true, get () {
         const value = $settings.value
-        Object.defineProperty(this, 'value', { enumerable: true, value: value, })
+        Object.defineProperty(this, 'value', { value: value, })
         return value
       } },
-      'valid': { configurable: true, enumerable: true, get () {
+      'valid': { configurable: true, get () {
         const valid = $settings.valid
-        Object.defineProperty(this, 'valid', { enumerable: true, value: valid })
+        Object.defineProperty(this, 'valid', { value: valid })
         return valid
       } },
 
