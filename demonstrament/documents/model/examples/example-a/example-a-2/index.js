761842bd43eb83f8aa4410379b51424e9c3d4a45
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-04-12 15:07:31 -0700

    Demonstrament Correction

--- demonstrament/documents/model/examples/example-a/example-a-2/index.js
+++ demonstrament/documents/model/examples/example-a/example-a-2/index.js
@@ -1,6 +1,17 @@
 console.log("--------")
 console.log("pushProp")
 console.log("--------")
+function recursiveSlice($object) {
+  if(!$object) return $object
+  const object = (Array.isArray($object)) ? $object.slice() : Object.create(Object.prototype, Object.getOwnPropertyDescriptors($object))
+  for(const [$propertyKey, $propertyValue] of Object.entries(object)) {
+    if(typeof $propertyValue === 'object') {
+      object[$propertyKey] = recursiveSlice($propertyValue)
+    }
+    else { object[$propertyKey] = $propertyValue }
+  }
+  return object
+}
 import { Model, Schema } from '/dependencies/objecture.js'
 import ComplexArrayA from '../../sets/complex-array-a/index.js'
 function eventLog($event) { console.log($event.type, $event.path) }
@@ -10,9 +21,9 @@ const model = new Model(ComplexArrayA, null, {
   assignObject: 'set', 
   enableEvents: true,
 })
-const object = Object.create(Object.prototype, Object.getOwnPropertyDescriptors(ComplexArrayA))
+const array = recursiveSlice(ComplexArrayA)
 const modelString = model.toString({ space: 2 })
-const objectString = JSON.stringify(object, null, 2)
+const arrayString = JSON.stringify(array, null, 2)
 console.log("modelString", modelString)
-console.log("objectString", objectString)
-console.log(modelString === objectString)
+console.log("arrayString", arrayString)
+console.log(modelString === arrayString)
